65c1709952e956e3b070ee90913b4f296bfe96d8
Matvey <83799026+Mioop75@users.noreply.github.com>
2022-02-04 16:48:57 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -34,3 +34,25 @@ games_clouse.addEventListener("click", function() {
     games_modal.classList.remove("on");
 })
 
+const scrollFunc = () => {
+   const links = document.querySelectorAll(".header a");
+
+   links.forEach((element) => {
+      element.addEventListener("click", (event) => {
+         event.preventDefault();
+
+         const id = element.getAttribute("href").substring(1);
+         const section = document.getElementById(id);
+         if(section) {
+            section.scrollIntoView({
+               behavior: "smooth",
+               block: "start"
+            })
+         } else {
+
+         }
+      })
+   })
+}
+
+scrollFunc();
